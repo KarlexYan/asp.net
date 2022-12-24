b7a111d51fdc9712811fd829b714b08721bba979
KarlexYan <KarlexYan@aliyun.com>
2022-12-24 17:06:36 +0800

    update

---
+++ WebApplication1/UserManage.cs
@@ -0,0 +1,23 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1
+{
+    public class UserManage
+    {
+        internal bool LoginSys(string username,string pwd,out string errmsg)
+        {
+            bool result = false;
+            errmsg = "登录失败";
+            string sqlstr = "select pwd,stuID from tb_student where userName = '" + username + "'";
+            MySqlConnection con = new MySqlConnection();
+            MySqlCommand cmd = new MySqlCommand();
+            con.ConnectionString = ConfigurationManager.AppSettings["RemoteConnectionString"];
+            return result;
+        }
+    }
+}
---
+++ WebApplication1/index.aspx
@@ -0,0 +1,6 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="WebApplication1.index" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <h1>欢迎来到刷题网站</h1>
+</asp:Content>
